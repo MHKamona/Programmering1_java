7214d3ff23e45f44cb36c48360d5dc1955841f73
MHKamona <m.h.kammona@gmail.com>
2024-02-05 20:18:47 +0100

    sida 29. book programmering 1 magnus lilja

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+//Skriv ett program som skapar de tre decimaltalen TalA, Talb Och summa, och som räknar ut summan.
+    public static void main(String[] args) {
+        double talA = 2.1;
+        double talB = 3.1;
+        double summa = talA + talB;
+
+        System.out.println("Tal A är " + talA );
+        System.out.println("Tal B är " + talB );
+        System.out.println("Summan är " + summa );
+    }
+}
